25a0f9f7b9f11d07c8cad044b824958bfc88e466
Nella1a <m.allen@gmx.at>
2022-06-24 21:42:12 +0200

    Add models

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,24 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+
+
+class ToDoList(models.Model):
+    title = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+      return self.name
+
+    class Meta:
+      ordering = ["date_created"]
+
+
+class ToDoEntry(models.Model):
+  todo_titel = models.CharField(max_length=100)
+  description = models.TextField
+  todo_list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+
+
+  def __str__(self):
+    return self.todo_titel
