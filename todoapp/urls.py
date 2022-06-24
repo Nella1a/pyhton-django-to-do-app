75c74047114b32051eaa2609cb987697b074ab26
Nella1a <m.allen@gmx.at>
2022-06-24 22:53:32 +0200

    Update urlconf and add for-loop

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,6 +1,7 @@
 from importlib.resources import path
 from django.urls import URLPattern, path
+from todoapp.views import AllListView
 
 urlpatterns = [
-
+  path("", AllListView.as_view(), name="index")
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,12 @@
+from msilib.schema import ListView
 from django.shortcuts import render
+from django.views.generic import ListView
+from todoapp.models import ToDoEntry, ToDoList
+
 
 # Create your views here.
+
+class AllListView(ListView):
+  model = ToDoList
+  template_name: "todoapp/index.html"
+  context_object_name: "all_lists"
